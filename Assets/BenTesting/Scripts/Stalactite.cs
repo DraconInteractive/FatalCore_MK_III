f08341c99a89f39a0d636440eda6213d6c65ef36
Xblivior <1001701@student.sae.edu.au>
2016-10-19 20:28:17 +0800

    Stalactite
    Stalactite works
    Need to finish OverlapSphere cast check on Stalactite Controller

---
+++ Assets/BenTesting/Scripts/Stalactite.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stalactite : MonoBehaviour 
+{
+	public bool playerClose = false;
+
+	public int damage;
+
+	//timers
+	float dropTimer;
+	float lifeTimer = 2;
+
+	//reference to rigidbody
+	public Rigidbody rB;
+
+	//player script reference
+	Player_Script ps;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//get player scripts
+		ps = Player_Script.playerObj.GetComponent<Player_Script> ();
+	
+	}
+
+	void Awake()
+	{
+		//set random drop time
+		dropTimer = Random.Range(1f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if playerClose == true
+		if (playerClose)
+		{
+			//start drop timer
+			dropTimer -= Time.deltaTime;
+
+			//if droptimer hits 0
+			if (dropTimer <= 0)
+			{
+				//enable gravity and disable kinematic
+				rB.useGravity = true;
+				rB.isKinematic = false;
+
+				//start life timer
+				lifeTimer -= Time.deltaTime;
+			}
+		}
+
+		//if life timer hits 0
+		if (lifeTimer <= 0)
+		{
+			//destroy gameobject
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//if it hits the player
+		if (other.gameObject == Player_Script.playerObj)
+		{
+			//damage player
+			ps.DamagePlayer (damage);
+		}
+	}
+}
---
+++ Assets/BenTesting/Scripts/StalactiteController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StalactiteController : MonoBehaviour 
+{
+	public GameObject[] stalactites;
+
+	Collider[] hitColliders;
+	public float checkRadius = 10;
+
+	float checkTimer = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		checkTimer -= Time.deltaTime;
+
+		if (checkTimer <= 0)
+		{
+			Check();
+			//checkTimer = 0.5f;
+		}
+	}
+
+	public void Check()
+	{
+		//do overlapsphere
+		//check for player
+		//if inside set stelectites playerClose = true
+		hitColliders = Physics.OverlapSphere(transform.position, checkRadius);
+
+		int i = 0;
+		while (i < stalactites.Length)
+		{
+			stalactites[i].GetComponent<Stalactite>().playerClose = true;
+			i++;
+		}
+
+		Destroy(gameObject);
+			
+	}
+}
