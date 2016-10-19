5ccf4fdf4481f1e3f32edc1d2d165a71c76e93f2
Xblivior <1001701@student.sae.edu.au>
2016-10-19 20:48:11 +0800

    Stalactites Work

--- Assets/BenTesting/Scripts/StalactiteController.cs
+++ Assets/BenTesting/Scripts/StalactiteController.cs
@@ -4,45 +4,60 @@ using System.Collections;
 public class StalactiteController : MonoBehaviour 
 {
 	public GameObject[] stalactites;
+	public float checkDistance = 10;
 
-	Collider[] hitColliders;
-	public float checkRadius = 10;
-
+	GameObject player;
 	float checkTimer = 0.5f;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		//set player object
+		player = Player_Script.playerObj;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		//start timer
 		checkTimer -= Time.deltaTime;
 
+		//if timer hits 0
 		if (checkTimer <= 0)
 		{
-			Check();
-			//checkTimer = 0.5f;
+			//call Drop
+			Drop();
+
+			//reset timer
+			checkTimer = 0.5f;
 		}
-	}
-
-	public void Check()
-	{
-		//do overlapsphere
-		//check for player
-		//if inside set stelectites playerClose = true
-		hitColliders = Physics.OverlapSphere(transform.position, checkRadius);
-
-		int i = 0;
-		while (i < stalactites.Length)
-		{
-			stalactites[i].GetComponent<Stalactite>().playerClose = true;
-			i++;
-		}
-
-		Destroy(gameObject);
 			
 	}
+
+	public void Drop()
+	{
+		//if distance between player and controller is <= checkDistance
+		if(Vector3.Distance(player.transform.position, transform.position) <= checkDistance)
+		{
+			//int
+			int i = 0;
+
+			//while i < number of stalactites
+			while (i < stalactites.Length)
+			{
+				//tell stalactite[i] to set playerclose to true
+				stalactites[i].GetComponent<Stalactite>().playerClose = true;
+
+				//add to i
+				i++;
+			}
+
+			//destroy controller
+			Destroy(gameObject);
+
+		}	
+
+	}
 }
+
+//Xblivior
