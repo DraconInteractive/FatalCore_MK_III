8d4484820399541f4e6f8cf2c5345a0a5e32bb90
Xblivior <1001701@student.sae.edu.au>
2016-10-21 17:42:34 +0800

    Spores started
    Spores detect player and look at him, but particle system doesnt play :(

--- Assets/BenTesting/Scripts/SporeController.cs
+++ Assets/BenTesting/Scripts/SporeController.cs
@@ -5,18 +5,43 @@ public class SporeController : MonoBehaviour
 {
 	public ParticleSystem spores;
 	public GameObject target;
-	float timer;
+
+	public float checkDistance;
+	float checkTimer = 0.5f;
+
+	 public GameObject player;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		//set player object
+		//player = Player_Script.playerObj;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		timer -= Time.deltaTime;
+		checkTimer -= Time.deltaTime;
+		if (checkTimer <= 0)
+		{
+			//if distance between player and controller is <= checkDistance
+			if(Vector3.Distance(player.transform.position, transform.position) <= checkDistance)
+			{
+				spores.Play();
+				target = player.gameObject;
+			}
+
+			else 
+			{
+				spores.Stop();
+				target = null;
+			}
+		}
+
+		if (target != null)
+		{
+			transform.LookAt(target.transform);
+		}
 
 	}
 }
